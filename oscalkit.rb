d3da26a049ec6789210928f7d76beb0ae0424857
OpenControl <dev@open-control.org>
2018-04-15 17:14:46 -0400

    Brew formula update for oscalkit version v0.1.0

--- oscalkit.rb
+++ oscalkit.rb
@@ -3,7 +3,7 @@ class Oscalkit < Formula
   homepage "https://github.com/opencontrol/oscalkit"
   url "https://github.com/opencontrol/oscalkit/releases/download/v0.1.0/oscalkit_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "d05df236f541f14994b8c1c2e1c6fa5a7c16fec704b705d6f20b3dd0fc5f5d93"
+  sha256 "73473044e94b9911e7e18596960c58ba275787c3f750884bb218ca5f2737beec"
   
   depends_on "libxml2"
 
